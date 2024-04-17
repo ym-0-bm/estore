e87d79c6ede768f0ea4b2b62867b2d317e6434fb
ym-0-bm <93938113+ym-0-bm@users.noreply.github.com>
2024-04-17 09:54:39 +0000

    mise à jour interface

---
+++ src/stock/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Stock(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, unique=True)
+    quantity = models.IntegerField(default=1)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
